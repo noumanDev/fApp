9fc998bbb5bf1f88acc373febdd78aa15500ee7c
Apple <apple@Apples-Mac-Pro.local>
2018-03-29 12:23:18 +0500

    deleted source file for movie app code

--- src/screens/index.js
+++
@@ -1,6 +0,0 @@
-import {Navigation} from 'react-native-navigation';
-import FeedsScreen from './FeedsScreen';
-
-export const registerScreens = ()=>{
-    Navigation.registerComponent('instagramclone.FeedsScreen',()=>FeedsScreen);
-}
